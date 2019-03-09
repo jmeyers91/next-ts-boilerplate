58141824ed3c32995ec51944c9913285ddcd061e
James Meyers <james.meyers@emberex.com>
2019-03-08 21:29:17 -0800

    rebuild

---
+++ client/dist/src/components/Column.js
@@ -0,0 +1,9 @@
+import styled from '../styled';
+export default styled('div') `
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
+  box-sizing: border-box;
+`;
+//# sourceMappingURL=Column.js.map
---
+++ client/dist/src/components/Row.js
@@ -0,0 +1,9 @@
+import styled from '../styled';
+export default styled('div') `
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  flex-shrink: 0;
+  box-sizing: border-box;
+`;
+//# sourceMappingURL=Row.js.map
